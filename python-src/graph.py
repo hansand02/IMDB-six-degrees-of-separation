efc142b63aff05f1171d63f6cc7b56219f8c5eb8
hansand02 <91554059+hansand02@users.noreply.github.com>
2022-11-06 18:03:07 +0100

    Ferdig nå, alle ønskede metoder funker, og går ganske så fort

--- python-src/graph.py
+++ python-src/graph.py
@@ -6,32 +6,43 @@ from itertools import chain
 from collections import deque
 import time  
 class Graph:
-
-    def __init__(self) -> None:
+    
+    def __init__(self, fast:bool) -> None:
+        # fast is very fast on testfile, but not 100% safe, but not fast is safe, and slower
         self.actorDict =  {}
         self.filmDict = {}
         self.hovedGraf = {}
         self.kanter = 0
         self.noder = 0
         self.currentVisitBoolean = True
+        if fast:
+            self.minimunResistance = 2.6
+        else:
+            self.minimunResistance =  0.8
 
-    #Returns nodes and edges actually in self.hovedGraf. Safe, but not fast
+    
     def kontrollerVerdier(self) -> tuple:
+        """ Returns nodes and edges actually in self.hovedGraf. Safe, but not fast """
         return(int(len(list(chain(*self.hovedGraf.values())))/2), len(self.hovedGraf.keys()))
     
-    #Returns the stored values for edges and nodes, fast, but not safe. 
+    
     def hentVerdier(self) -> tuple:
+        """ Returns the stored values for edges and nodes, fast, but not safe.  """
         return (int(self.kanter/2), self.noder)
     
-    #Simplifying external use of reading from file
-    def les(self, movies:str, actors:str) -> None:
+    
+    def lesOgLag(self, movies:str, actors:str) -> None:
+        """ Simplifying external use of reading from file and building """
         self.lesFilmer(movies)
         self.lesActors(actors)
+        self.lagGraf()
 
-    #self.actorDict becomes key, value = actorId, ActorObject
+    
     def lesActors(self, filnavn: str) -> None:
-        #Open file, read actors by id, name and movies played in
-        #Create new Actor object from this, iterate over all movies played in, and add this object in the actors list in Movie. 
+        """ self.actorDict becomes key, value = actorId, ActorObject 
+        Open file, read actors by id, name and movies played in
+        Create new Actor object from this, iterate over all movies played in, and add this object in the actors list in Movie. 
+        """
         with open(filnavn, encoding="utf-8") as fil:
             for linje in fil:
                 if linje:
@@ -44,8 +55,9 @@ class Graph:
                         if title in self.filmDict:
                             self.filmDict[title].actors.append(skuespiller)
 
-    #creates self.filmDict a dictionary key,value = movieId, MovieObject
+    
     def lesFilmer(self, filnavn: str) -> None:
+        """ Creates self.filmDict, a dictionary with key,value = movieId, MovieObject """
         try:
             assert len(self.actorDict) == 0
         except: 
@@ -57,9 +69,9 @@ class Graph:
                     linjeL = linje.strip().split("\t")
                     self.filmDict[linjeL[0]] = Movie(linjeL[0], linjeL[1], float(linjeL[2]))
 
-    #creates self.hovedGraf: key,value = ActorObject, ListOfTuples -> [(ActorObject, MovieObject), ..., ...,]
+    
     def lagGraf(self) -> None:
-        
+        """ creates self.hovedGraf: key,value = ActorObject, ListOfTuples -> [(ActorObject, MovieObject), ..., ...,] """
         self.hovedGraf = {k:[] for k in self.actorDict.values()}
         for movie in self.filmDict.values():
             filmListe = []
@@ -71,8 +83,9 @@ class Graph:
                 self.kanter += len(filmListe)
                 filmListe.append((actor, movie))
 
-    #Makes visual print of the path
+    
     def printGrafSti(self, pathList:list) -> None:
+        """ Makes visual print of the path between two actors"""
         print("=" *35)
         for index, actor in enumerate(pathList):
             
@@ -84,7 +97,7 @@ class Graph:
                     print(" " *20 , actor)
                     return print("="* (21 + len(actor.name)))       
                 if not isRightSide:
-                    print(actor, actor.totalWeight)
+                    print(actor)
                     return print("="* (21 + len(actor.name)))         
 
             if isRightSide:
@@ -102,12 +115,14 @@ class Graph:
                         continue
                     print(" "*spaces + " "*i + "\\" )
     
-    #"Resets" graph so BFSfull or nyDijkstra can be run back to back on the same graph
-    def resettGraf(self):
+    
+    def resettGraf(self) -> None:
+        """ "Resets" graph, False and True as visited boolean, so BFSfull or nyDijkstra can be run back to back on the same graph """
         self.currentVisitBoolean = not self.currentVisitBoolean
     
-    #Finds the actual actor objects that correspond to input ids.
+    
     def lagActorFraId(self, nmIdStart:str,nmIdSlutt:str) -> tuple:
+        """ Finds the actual actor objects that correspond to input ids. """
         for actor in self.hovedGraf.keys():
             #Both if in case start and end is the same
             if actor.id == nmIdSlutt:
@@ -116,9 +131,9 @@ class Graph:
                 startNode = actor 
         return (startNode, sluttNode)
     
-    #This method does the unwegihted BFS search (OPPGAVE 2)
+    #(OPPGAVE 2)
     def BFSfull(self,  nmIdStart:str,nmIdSlutt:str):
-        
+            """ This method does the unwegihted BFS search on self.hovedGraf  """
             tuple = self.lagActorFraId(nmIdStart, nmIdSlutt)
             startNode, sluttNode = tuple[0], tuple[1]
             queue = deque()
@@ -156,9 +171,12 @@ class Graph:
                         queue.append(naboer[0])
             print("finnes ingen vei")
 
-    #Works very well, very fast and works with the same path several times in a role 
+    #(OPPGAVE 3)
     def nyDijkstra(self, nmIdStart:str, nmIdSlutt:str) -> None:
-        
+
+        """ Finds the path between with the least resistance (best rated movies)
+        Works with the same path several times in a row """
+
         actors = self.lagActorFraId(nmIdStart, nmIdSlutt)
         startNode, sluttNode = actors[0],actors[1]
         startNode.visited, startNode.forrige, startNode.totalWeight, sluttNode.totalWeight = self.currentVisitBoolean, None, 0, 10
@@ -173,7 +191,8 @@ class Graph:
             poppetNode = que.popleft()
             #Since the lowest possible resistance is 10-9.2 = 0.8; n = 0.8 is the safe number
             #But for runtimeflex put n = 2.6 
-            if poppetNode.totalWeight+ 2.6> sluttNode.totalWeight:   
+            #Number made in def init
+            if poppetNode.totalWeight+ self.minimunResistance> sluttNode.totalWeight:   
                 continue
 
             for tuples in self.hovedGraf[poppetNode]:
@@ -207,10 +226,50 @@ class Graph:
                 self.printGrafSti(pathList)
                 self.resettGraf()
                 print(f"Total weight: {popNode.totalWeight}\n")
-                """ for actors in self.hovedGraf.keys():
-                    actors.totalWeight = 100 """
                 return
             popNode.totalWeight = 10
+
+    #(OPPGAVE 4)
+    def findComponents(self):
+        """ Method that finds the number of componetns and the size of said components in hovedgraf """
+        #Dict with key, value = size, numberOfComponents
+        componentMap = {}
+        
+        try:
+            assert any(self.hovedGraf)
+        except:
+            print("Kan ikke finne komponentene på en tom graf..")
+        
+        #Assume this is neccessary because i have som kind of bug that makes some of the keys visited
+        for actors in self.hovedGraf.keys():
+            actors.visited = not self.currentVisitBoolean
+
+        for actor in self.hovedGraf.keys():  
+            if actor.visited == self.currentVisitBoolean:
+                continue
+            stack = deque()
+            stack.append(actor)
+            teller = 1
+
+            while stack:
+                skuespiller = stack.pop()
+                
+                for actorTuple in self.hovedGraf[skuespiller]:
+                    if not actorTuple[0].visited == self.currentVisitBoolean:
+                        stack.append(actorTuple[0])
+                        skuespiller.visited = self.currentVisitBoolean
+                        actorTuple[0].visited = self.currentVisitBoolean
+                        teller += 1
+
+            if teller not in componentMap:
+                componentMap[teller] = 1
+            else: componentMap[teller] += 1
+
+        
+        for items in sorted(componentMap.items(), reverse=True):  
+            print(f"There are {items[1]} components of size {items[0]}")
+
+        self.resettGraf()
 
 
 
--- python-src/innleveringsoppgave3.py
+++ python-src/innleveringsoppgave3.py
@@ -1,43 +1,58 @@
 from graph import Graph
 import timeit 
-import dis
-import time
-EXPECTEDVALUES = (5068918,119205)
 
+EXPECTEDVALUES = (5068918,119205)
 
 if __name__ == "__main__":
 
-    #To make a boolean for graph validity
-    #A double check to the already implemented "test()"
-    def sjekk(graf:Graph, verdier:tuple) -> bool:
-        return graf.kontrollerVerdier() == EXPECTEDVALUES
+    
+    ans = input("Fast or safe? (f/s)").lower()
 
-    def arbeid() -> None:
-        oppgave2 = Graph()
-        
-        def bygg():    
-            oppgave2.les("../data/movies.tsv", "../data/actors.tsv")
-            oppgave2.lagGraf()
-        
+    def total() -> None:
+        oppgave2 = Graph(fast= ans == "f")  
+
         def Hjelpefunksjon():
-            print("Oppgave 1\n")
-            stringBygg = f"{timeit.timeit(bygg, number=1):.5f}s to build graph"
-            stringTest = f"{timeit.timeit(test, number=1):.5f}s to validate graph"
-            print("\n"+stringBygg)
-            print(stringTest+ "\n")
-            print("Oppgave 2")
-            stringSearch = f"{timeit.timeit(searchUnweighted, number=1):.5f}s to search shortest path x5"
-            print(stringSearch+"\n")
-            stringSearchWeighted = f"{timeit.timeit(searchWeighted, number=1):.5f}s to search easiest path x5"
-            print(stringSearchWeighted + "\n")
+            
+            input("Oppgave 1: \nTrykk enter!:")
+            print()
+            floatBygg =  timeit.timeit(bygg, number=1)
+            floatTest = timeit.timeit(test, number=1)
+            print(f"\n{floatBygg:.5f}s to build graph")
+            print(f"{floatTest:.5f}s to validate graph\n")
+            
+            
+            input("Oppgave 2\nTrykk enter!:")
+            floatSearch = timeit.timeit(searchUnweighted, number=1)
+            print(f"{floatSearch:.5f}s to search shortest path x5\n")
+            
+            input("Oppgave 3\nTrykk enter!:")
+            floatSearchWeighted = timeit.timeit(searchWeighted, number=1)
+            print(f"{floatSearchWeighted:.5f}s to search easiest path x5\n")
+    
+            input("Oppgave 4\nTrykk enter!:")
+            floatKomponent = timeit.timeit(findComponents, number=1)
+            print("="*45)
+            print(f"{floatKomponent:.5f}s to find components\n")
 
+            print("="*56)
+            print(f"{(floatBygg+floatTest+floatSearch+floatSearchWeighted+ floatKomponent):.5f} s for å prossesere oppgave 1-4")
+
+        def bygg():  
+            """ Oppgave 1 """  
+            oppgave2.lesOgLag("../data/movies.tsv", "../data/actors.tsv")
+        
         def test():
+            """ Oppgave 1 """
             verdier = oppgave2.hentVerdier()
             print(f"Nodes: {verdier[1]} \nEdges: {verdier[0]}")
             assert verdier == EXPECTEDVALUES
-        
-        
+      
+        def sjekkTest(graf:Graph, verdier:tuple) -> bool:
+            """ More accurate extension of test, doublecheck the validity of test """
+            return graf.kontrollerVerdier() == EXPECTEDVALUES
+
         def searchUnweighted():
+            """ Oppgave 2 """
             oppgave2.BFSfull("nm2255973","nm0000460")  
             oppgave2.BFSfull("nm0424060","nm0000243")  
             oppgave2.BFSfull("nm4689420","nm0000365")  
@@ -45,15 +60,18 @@ if __name__ == "__main__":
             oppgave2.BFSfull("nm0031483","nm0931324")  
 
         def searchWeighted(): 
+            """ Oppgave 3 """
             oppgave2.nyDijkstra("nm2255973","nm0000460")   
             oppgave2.nyDijkstra("nm0424060","nm0000243")  
             oppgave2.nyDijkstra("nm4689420","nm0000365")  
             oppgave2.nyDijkstra("nm0000288","nm0001401") 
             oppgave2.nyDijkstra("nm0031483","nm0931324")  
-            #oppgave2.nyDijkstra("nm0424216", "nm0697611" )
              
-             
-             
+        def findComponents():
+            oppgave2.findComponents()
+
         Hjelpefunksjon()
-    stringTotal = (f"{timeit.timeit(arbeid, number=1):.5f}s total")
-    print(stringTotal, "For oppgave 1, 2 og 3 (2 og 3 fem ganger)")
+
+    stringTotal = (f"Men {timeit.timeit(total, number=1):.5f}s tok det totalt med litt OStime og din input")
+    print(stringTotal)
+    print("="*56)
